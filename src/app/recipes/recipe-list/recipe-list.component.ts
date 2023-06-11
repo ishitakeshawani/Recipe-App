2f65468febecf9a470a0fddb9efa678b350dc6f4
Ishita <ishita.k@turvo.com>
2023-06-11 17:29:04 +0530

    Made components for recipe detail, recipe list, recipe item and shopping list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('test',"test123","https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70")
+  ];
+
+}
